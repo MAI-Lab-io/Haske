52f3158c3d3c07cf4f43bafb5f6152e3227f2536
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2024-11-07 16:07:04 +0100

    Update ProtectedContent.js

--- auth/src/components/ProtectedContent.js
+++ auth/src/components/ProtectedContent.js
@@ -16,7 +16,7 @@ function ProtectedContent() {
     return (
         <div className="protected-container">
             <iframe
-                src="" // Replace with your actual URL
+                src="http://51.44.123.184:5000/ui/app/#/" // Replace with your actual URL
                 title="Protected Content"
                 className="protected-iframe"
             ></iframe>
