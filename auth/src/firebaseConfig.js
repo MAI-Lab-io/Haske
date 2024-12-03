9233aa2fb3e9366411a11dca04c2cda62b96824b
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2024-12-03 16:05:58 +0100

    Update firebaseConfig.js

--- auth/src/firebaseConfig.js
+++ auth/src/firebaseConfig.js
@@ -14,9 +14,9 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const firestore = getFirestore(app);
 const auth = getAuth(app);
 
 
-export { db, auth };
+export { db, firestore };
 
