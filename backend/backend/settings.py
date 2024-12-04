fdb695de1985852518fc7c20a4d8305945423f1f
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2024-12-04 12:41:29 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# Path where Django will collect static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
