e13d5c035b97a179666e7b54e428a0b1cbd8a23e
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2024-12-04 13:57:32 +0100

    Update urls.py

--- backend/verification/urls.py
+++ backend/verification/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import submit_verification, get_users, approve_user
+from .views import submit_verification, get_users, approve_user, is_verified
 
 urlpatterns = [
     path("submit-verification/", submit_verification, name="submit_verification"),
     path("get-users/", get_users, name="get_users"),
+    path("is-verified/", is_verified, name="is_verified"),
     path("approve-user/<int:user_id>/", approve_user, name="approve_user"),
 ]
