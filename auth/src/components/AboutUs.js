c32d9507a73f2ef09955d1ac9a87385a38f9b42a
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-01-29 14:44:59 +0100

    Update AboutUs.js

--- auth/src/components/AboutUs.js
+++ auth/src/components/AboutUs.js
@@ -1,4 +1,4 @@
-import React from "react";
+cimport React from "react";
 import "./AboutUs.css";
 
 const AboutUs = () => {
@@ -8,13 +8,14 @@ const AboutUs = () => {
         <h1 className="about-us-title">About Us</h1>
         <p className="about-us-description">
           We are a team of passionate professionals committed to advancing
-          science and technology in the field of [Your Field]. Our mission is to
-          [Your Mission], and we strive to make a positive impact on society
+          science and technology in medical Imaging. Our mission is to
+          empower medical imaging workflow through seamless archival process and integration with Artificial Intelligence, and we strive to make a positive impact on society
           through our research, projects, and collaborations.
         </p>
         <p className="about-us-mission">
           <strong>Our Mission:</strong> To drive innovation and excellence in
-          [specific mission statement].
+          At Haske, our mission is to revolutionize the healthcare landscape in low-resource environments like Nigeria by providing accessible, affordable, and efficient medical imaging solutions. We aim to empower Radiology departments with AI-powered, web-based Picture Archiving and Communication Systems (PACS) that meet international standards. 
+          Through our platform, we seek to enhance diagnostic capabilities, improve healthcare outcomes, and promote equitable access to high-quality medical care across underserved regions..
         </p>
         <p className="about-us-values">
           <strong>Our Values:</strong> Integrity, Collaboration, Innovation, and
