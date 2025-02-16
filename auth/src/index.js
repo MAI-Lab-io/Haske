67f3930e01527fc9837778b21f6b1f2eb0448d5b
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-02-16 16:20:00 +0100

    Update index.js

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./style/flexboxgrid.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './style/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
