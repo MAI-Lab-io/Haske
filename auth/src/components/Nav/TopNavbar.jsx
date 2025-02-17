2d1e2e3a8b0dbcbc72bfd79a81893330fc1ff14c
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-02-17 13:22:16 +0100

    Update TopNavbar.jsx

--- auth/src/components/Nav/TopNavbar.jsx
+++ auth/src/components/Nav/TopNavbar.jsx
@@ -74,7 +74,7 @@ export default function TopNavbar() {
               </a>
             </li>
             <li className="semiBold font15 pointer flexCenter">
-              <a href="/" className="radius8 lightBg" style={{ padding: "10px 15px" }}>
+              <a href="/" className="radius8 darkBg" style={{ padding: "10px 15px" }}>
                 Get Started
               </a>
             </li>
