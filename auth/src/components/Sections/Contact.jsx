aaf0990aab9f86c1b58a566381d57ece60ec311c
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-02-17 12:17:53 +0100

    Update Contact.jsx

--- auth/src/components/Sections/Contact.jsx
+++ auth/src/components/Sections/Contact.jsx
@@ -1,29 +1,23 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-scroll";
 // Assets
-import LogoImg from "../../assets/haske.png";
-
+import ContactImg1 from "../../assets/img/contact-1.png";
+import ContactImg2 from "../../assets/img/contact-2.png";
+import ContactImg3 from "../../assets/img/contact-3.png";
 
 export default function Contact() {
-  const getCurrentYear = () => {
-    return new Date().getFullYear();
-  };
-
   return (
-    <Wrapper>
+    <Wrapper id="contact">
       <div className="lightBg">
         <div className="container">
           <HeaderInfo>
-            <h1 className="font40 extraBold">We’d Love to Partner with You</h1>
+            <h1 className="font40 extraBold">Partner with Us</h1>
             <p className="font13">
-              At Haske, we are actively seeking institutions interested in integrating advanced digital imaging solutions. 
-              If you would like to explore how Haske can support your institution, or if you're interested in a partnership, 
-              please get in touch with us.
-            </p>
-            <p className="font13">
-              Do you currently use digitizers or any form of digital imaging tools in your facility? If so, we would love to hear 
-              about the tools you use and how we might be able to enhance your current setup with Haske.
+              Is your institution looking to integrate innovative digital imaging and diagnostic tools?
+              <br />
+              At Haske, we are committed to enhancing operational efficiency and advancing medical diagnostics. If your institution is interested in exploring a partnership or implementing Haske in your operations, we would be delighted to discuss how we can support your goals.
+              <br />
+              Additionally, we are curious to know if your institution currently utilizes any digitizers or digital imaging tools. This will help us understand how best to tailor our solutions to fit your needs.
             </p>
           </HeaderInfo>
           <div className="row" style={{ paddingBottom: "30px" }}>
@@ -31,18 +25,14 @@ export default function Contact() {
               <Form>
                 <label className="font13">Institution Name:</label>
                 <input type="text" id="institution" name="institution" className="font20 extraBold" />
-                
-                <label className="font13">Contact Person's Full Name:</label>
+                <label className="font13">Contact Person:</label>
                 <input type="text" id="contact" name="contact" className="font20 extraBold" />
-                
                 <label className="font13">Email Address:</label>
                 <input type="email" id="email" name="email" className="font20 extraBold" />
-                
-                <label className="font13">Are you currently using any digital imaging tools or digitizers in your institution?</label>
-                <textarea rows="4" cols="50" type="text" id="tools" name="tools" className="font20 extraBold" placeholder="Please provide details of the tools you currently use." />
-
-                <label className="font13">Inquiry/Request Details:</label>
-                <textarea rows="6" cols="50" type="text" id="inquiry" name="inquiry" className="font20 extraBold" placeholder="Please describe your inquiry or request for partnership." />
+                <label className="font13">Do you currently utilize any digitizers or digital imaging tools in your institution?</label>
+                <textarea rows="4" cols="50" type="text" id="tools" name="tools" className="font20 extraBold" placeholder="Please specify the tools your institution currently uses." />
+                <label className="font13">Additional Inquiry or Comments:</label>
+                <textarea rows="4" cols="50" type="text" id="message" name="message" className="font20 extraBold" placeholder="Feel free to ask any specific questions or provide additional information." />
               </Form>
               <SumbitWrapper className="flex">
                 <ButtonInput type="submit" value="Submit Inquiry" className="pointer animate radius8" style={{ maxWidth: "220px" }} />
@@ -66,25 +56,6 @@ export default function Contact() {
           </div>
         </div>
       </div>
-      <FooterWrapper>
-        <div className="container">
-          <InnerWrapper className="flexSpaceCenter" style={{ padding: "30px 0" }}>
-            <Link className="flexCenter animate pointer" to="home" smooth={true} offset={-80}>
-              <img src={LogoImg} alt="Haske Logo" />
-              <h1 className="font15 extraBold whiteColor" style={{ marginLeft: "15px" }}>
-                Haske
-              </h1>
-            </Link>
-            <StyleP className="whiteColor font13">
-              © {getCurrentYear()} - <span className="purpleColor font13">Haske</span> All Rights Reserved
-            </StyleP>
-
-            <Link className="whiteColor animate pointer font13" to="home" smooth={true} offset={-80}>
-              Back to top
-            </Link>
-          </InnerWrapper>
-        </div>
-      </FooterWrapper>
     </Wrapper>
   );
 }
@@ -143,25 +114,5 @@ const SumbitWrapper = styled.div`
   @media (max-width: 991px) {
     width: 100%;
     margin-bottom: 50px;
-  }
-`;
-
-const FooterWrapper = styled.div`
-  background-color: #262626;
-  padding: 30px 0;
-`;
-
-const InnerWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  @media (max-width: 550px) {
-    flex-direction: column;
-  }
-`;
-
-const StyleP = styled.p`
-  @media (max-width: 550px) {
-    margin: 20px 0;
   }
 `;
