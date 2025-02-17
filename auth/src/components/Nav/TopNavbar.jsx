f63c12d5c10f0782fbf84cee31ed3344ff544b2c
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-02-17 14:51:09 +0100

    Update TopNavbar.jsx

--- auth/src/components/Nav/TopNavbar.jsx
+++ auth/src/components/Nav/TopNavbar.jsx
@@ -75,9 +75,10 @@ const Wrapper = styled.nav`
   top: 0;
   left: 0;
   z-index: 999;
-  background: linear-gradient(90deg, #eff5ff 0%, #5b617a 100%);
+  background: linear-gradient(90deg,  #ffffff 0%,  #eff5ff 50%, #5b617a 100%);
   transition: background 0.3s ease-in-out;
 `;
+
 
 
 const NavInner = styled.div`
