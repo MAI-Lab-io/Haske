409b4fbd2e9348e01cdfc72cadf6432708074a4f
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-02-17 12:18:52 +0100

    Update Footer.jsx

--- auth/src/components/Sections/Footer.jsx
+++ auth/src/components/Sections/Footer.jsx
@@ -16,13 +16,13 @@ export default function Contact() {
         <div className="container">
           <InnerWrapper className="flexSpaceCenter" style={{ padding: "30px 0" }}>
             <Link className="flexCenter animate pointer" to="home" smooth={true} offset={-80}>
-              <LogoImg />
+              <LogoImage src={LogoImg} alt="Haske Logo" />
               <h1 className="font15 extraBold whiteColor" style={{ marginLeft: "15px" }}>
                 Fanatic
               </h1>
             </Link>
             <StyleP className="whiteColor font13">
-              © {getCurrentYear()} - <span className="purpleColor font13">Haske</span> All Right Reserved
+              © {getCurrentYear()} - <span className="purpleColor font13">Haske</span> All Rights Reserved
             </StyleP>
 
             <Link className="whiteColor animate pointer font13" to="home" smooth={true} offset={-80}>
@@ -38,12 +38,24 @@ export default function Contact() {
 const Wrapper = styled.div`
   width: 100%;
 `;
+
 const InnerWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   @media (max-width: 550px) {
     flex-direction: column;
+    text-align: center;
   }
 `;
+
+const LogoImage = styled.img`
+  max-width: 100px;
+`;
+
 const StyleP = styled.p`
+  font-size: 13px;
+  margin: 0;
   @media (max-width: 550px) {
     margin: 20px 0;
   }
