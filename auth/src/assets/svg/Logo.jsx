ae0789b40523da02aed59204cd7f140644190539
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-02-17 11:54:50 +0100

    Update Logo.jsx

--- auth/src/assets/svg/Logo.jsx
+++ auth/src/assets/svg/Logo.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 function SvgComponent(props) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={27} height={40} viewBox="0 0 27 40" {...props}>
-      <image href="logo.png" x="0" y="0" width="27" height="40" />
+      <image href="../svg/logo.png" x="0" y="0" width="27" height="40" />
     </svg>
   );
 }
