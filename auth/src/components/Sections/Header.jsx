38208797b67e5893d04f2e92e9b578b92bb74705
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-02-21 17:03:10 +0100

    Update Header.jsx

--- auth/src/components/Sections/Header.jsx
+++ auth/src/components/Sections/Header.jsx
@@ -3,7 +3,7 @@ import styled, { keyframes } from "styled-components";
 // Components
 import FullButton from "../Buttons/FullButton";
 // Assets
-import HeaderImage from "../../assets/img/header-imgg.png";
+import HeaderImage from "../../assets/img/header-img.png";
 import QuotesIcon from "../../assets/svg/Quotes";
 import Dots from "../../assets/svg/Dots";
 
